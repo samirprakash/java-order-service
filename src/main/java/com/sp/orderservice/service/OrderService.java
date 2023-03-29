5a852fba1e36cdff55ef7a49287eb18f40a767cc
Samir Prakash <Samir.Prakash@gmail.com>
2023-03-28 22:49:52 -0500

    WebClient connection established from order to inventory service for stock checking

--- src/main/java/com/sp/orderservice/service/OrderService.java
+++ src/main/java/com/sp/orderservice/service/OrderService.java
@@ -1,11 +1,14 @@
 package com.sp.orderservice.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.sp.orderservice.dto.InventoryResponse;
 import com.sp.orderservice.dto.OrderLineItemsDTO;
 import com.sp.orderservice.dto.OrderRequest;
 import com.sp.orderservice.model.Order;
@@ -20,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
     
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -27,7 +31,22 @@ public class OrderService {
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDTOList().stream().map(orderLineItemsDTO -> mapOrderLineItems(orderLineItemsDTO)).toList();
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
+
+        // Call inventory service to check if the product is available.
+        // This is an example of synchronous call as we are using .block() method.
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                            .uri("http://localhost:8082/api/v1/inventory", uribuilder -> uribuilder.queryParam("skuCode", skuCodes).build())
+                            .retrieve()
+                            .bodyToMono(InventoryResponse[].class)
+                            .block();
+        
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+        if (!allProductsInStock) {
+            throw new IllegalArgumentException("Product is not in stock!");
+        } else {
+            orderRepository.save(order);
+        }
     }
 
     private OrderLineItems mapOrderLineItems(OrderLineItemsDTO orderLineItemsDTO) {
