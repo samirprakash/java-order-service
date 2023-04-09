7e05474c5ce9d8602cf4d8b27661d5cd44fd3769
Samir Prakash <Samir.Prakash@gmail.com>
2023-04-08 21:36:27 -0500

    implemeted circuit breaker

--- src/main/java/com/sp/orderservice/controller/OrderController.java
+++ src/main/java/com/sp/orderservice/controller/OrderController.java
@@ -10,19 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sp.orderservice.dto.OrderRequest;
 import com.sp.orderservice.service.OrderService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/order")
 @RequiredArgsConstructor
 public class OrderController {
-    
+
     private final OrderService orderService;
-    
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallback")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order created";
+    }
+
+    public String placeOrderFallback(OrderRequest orderRequest, RuntimeException e) {
+        return "Order creation failed";
     }
 }
